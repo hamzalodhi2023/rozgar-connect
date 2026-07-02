85dd195bb8f4c38d6d1e6017dbf40ae8f20c5554
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-07-02 17:08:47 +0500

    test: add Playwright test suite for worker ID verification process

--- frontend/tests/idVerification.spec.ts
+++ frontend/tests/idVerification.spec.ts
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
     await page.getByRole('link', { name: 'Sign In' }).click();
     await page.getByRole('textbox', { name: 'john@example.com' }).click();
     await page.getByRole('textbox', { name: 'john@example.com' }).fill('hamzalodhi2023@gmail.com');
-    await page.getByRole('textbox', { name: 'john@example.com' }).press('Tab');
+    await page.getByRole('textbox', { name: '••••••••' }).click();
     await page.getByRole('textbox', { name: '••••••••' }).fill('123456789');
     await page.getByRole('button', { name: 'Sign In' }).click();
     await page.getByRole('button', { name: 'H Hamza Khan Lodhi' }).click();
