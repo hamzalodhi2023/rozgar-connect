db47b3e3678bda330ae8b8550d7c8762a4410185
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-07-03 10:52:49 +0500

    test: add Playwright integration test for worker ID verification flow

--- frontend/tests/idVerification.spec.ts
+++ frontend/tests/idVerification.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-    await page.goto("/");
+    // await page.goto("/");
+    await page.goto('http://192.168.20.148:5173/');
     await page.getByRole('link', { name: 'Sign In' }).click();
     await page.getByRole('textbox', { name: 'john@example.com' }).click();
     await page.getByRole('textbox', { name: 'john@example.com' }).fill('hamzalodhi2023@gmail.com');
-    await page.getByRole('textbox', { name: '••••••••' }).click();
+    await page.getByRole('textbox', { name: 'john@example.com' }).press('Tab');
     await page.getByRole('textbox', { name: '••••••••' }).fill('123456789');
     await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.getByRole('button', { name: 'H Hamza Khan Lodhi' }).click();
-    await page.getByRole('link', { name: 'Worker Dashboard' }).click();
+    await page.getByRole('button', { name: 'Become a Worker' }).click();
     await page.getByRole('link', { name: 'Worker Profile' }).click();
     await page.getByText('Change Front').click();
     await page.getByLabel('Change Front').setInputFiles('tests/assets/idcard.jpg');
