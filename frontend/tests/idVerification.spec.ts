271ed9877c6b53215a23d9ddbcb408f649e3512d
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-07-03 10:56:18 +0500

    test: add playwright test case for id verification flow

--- frontend/tests/idVerification.spec.ts
+++ frontend/tests/idVerification.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-    // await page.goto("/");
-    await page.goto('http://192.168.20.148:5173/');
+    await page.goto('/');
+    // await page.goto('http://192.168.20.148:5173/');
     await page.getByRole('link', { name: 'Sign In' }).click();
     await page.getByRole('textbox', { name: 'john@example.com' }).click();
     await page.getByRole('textbox', { name: 'john@example.com' }).fill('hamzalodhi2023@gmail.com');
